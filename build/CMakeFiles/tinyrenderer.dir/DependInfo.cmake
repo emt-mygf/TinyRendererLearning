7eef0ecf1962430567e21ab9f3768f42ccadfdd4
hutao's ghost <emilia.m.t@qq.com>
2023-07-02 17:32:40 +0800

    Lesson 0

---
+++ build/CMakeFiles/tinyrenderer.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/whj/data/GitFiles/TinyRendererLearning/main.cpp" "CMakeFiles/tinyrenderer.dir/main.cpp.obj" "gcc" "CMakeFiles/tinyrenderer.dir/main.cpp.obj.d"
+  "D:/whj/data/GitFiles/TinyRendererLearning/tgaimage.cpp" "CMakeFiles/tinyrenderer.dir/tgaimage.cpp.obj" "gcc" "CMakeFiles/tinyrenderer.dir/tgaimage.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tinyrenderer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyrenderer.dir/main.cpp.obj"
+  "CMakeFiles/tinyrenderer.dir/main.cpp.obj.d"
+  "CMakeFiles/tinyrenderer.dir/tgaimage.cpp.obj"
+  "CMakeFiles/tinyrenderer.dir/tgaimage.cpp.obj.d"
+  "libtinyrenderer.dll.a"
+  "tinyrenderer.exe"
+  "tinyrenderer.exe.manifest"
+  "tinyrenderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyrenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
